126286375109992dbbf6bee47cd800704cc6205e
David Romero <romeroad@sheridancollage.ca>
2021-02-26 12:28:09 -0500

    added input output files

---
+++ src/main/java/sheridan/romeroad/midtermv2/midtermv2/MidtermV2Application.java
@@ -0,0 +1,15 @@
+package sheridan.romeroad.midtermv2.midtermv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MidtermV2Application.class, args);
+    }
+
+
+
+}
---
+++ src/test/java/sheridan/romeroad/midtermv2/midtermv2/MidtermV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.romeroad.midtermv2.midtermv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
